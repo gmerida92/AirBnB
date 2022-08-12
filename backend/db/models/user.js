542a463aecc2be90b34b240f9f1dc8611d091cfe
George Merida <gmerida92@gmail.com>
2022-08-11 20:28:32 -0700

    Add User model authentication Sequelize scopes and methods

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,18 +1,58 @@
 'use strict';
 const { Model, Validator } = require('sequelize');
+const bscrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
+
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
+    toSafeObject() {
+      const { id, username, email } = this; // context will be the User Instance
+      return { id, username, email };
+    }
+
+    validatePassword(password) {
+      return bcrypt.compareSync(password, this.hashedPassword.toString());
+    }
+
+    static getCurrentUserById(id) {
+      return User.scope("currentUser").findByPk(id);
+    }
+
+    // Login method
+    static async login({ credential, password }) {
+      const { Op } = require('sequelize');
+      const user = await User.scope('loginUser').findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential
+          }
+        }
+      });
+      if (user && user.validatePassword(password)) {
+        return await User.scope('currentUser').findByPk(user.id);
+      }
+    }
+
+    // Sign up method
+    static async signup({ username, email, password }) {
+      const hashedPassword = bscrypt.hashSync(password);
+      const user = await User.create({
+        username,
+        email,
+        hashedPassword
+      });
+      return await User.scope('currentUser').findByPk(user.id)
+    }
+
     static associate(models) {
       // define association here
     }
   }
+
   User.init({
+
     firstName: {
       type: DataTypes.STRING,
       allowNull: false
@@ -24,7 +64,7 @@ module.exports = (sequelize, DataTypes) => {
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate:{
+      validate: {
         len: [3, 256],
         isEmail: true
       }
@@ -51,6 +91,20 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ["id", "email", "password", "createdAt", "updatedAt"]
+      }
+    },
+    scopes: {
+      currentUser: {
+        attributes: { exclude: ["password", "createdAt", "updatedAt"] }
+      },
+      loginUser: {
+        attributes: { exclude: ["createdAt", "updatedAt"] }
+      }
+    }
   });
+
   return User;
 };
