4b9c8c7d4b2f3694faccf387b6522d9436ad8481
George Merida <gmerida92@gmail.com>
2022-08-14 13:04:38 -0700

    edit

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,8 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
-
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
-
 const { User } = require('../../db/models');
+
 
 const usersRouter = require('./users.js');
 
