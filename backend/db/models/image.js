9d5d2b6d6d6088d2014e80682d249a5ef4826655
George Merida <gmerida92@gmail.com>
2022-08-14 21:22:49 -0700

    create images-model-and-migrations

---
+++ backend/db/models/image.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class image extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  image.init({
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    imageableId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    imageableType: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'image',
+  });
+  return image;
+};
