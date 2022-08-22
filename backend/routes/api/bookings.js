7ff73207019954af730b45fe85b1ae4264f62304
George Merida <gmerida92@gmail.com>
2022-08-21 20:44:29 -0700

    bookings route queries complete

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -6,5 +6,34 @@ const { Op } = require("sequelize");
 const { validateSpot, validateReview, validateEndDate, validateQueryParameters } = require('../../utils/validation_req_body.js');
 const router = express.Router();
 
+// Update and return existing booking
+router.put('/:id', [restoreUser, requireAuthentication, requireAuthorizationUserBooking, validateEndDate], async (req, res) =>{
+    const user = req.user;
+    const {id} = req.params;
+    const {startDate, endDate} = req.body;
+
+    const booking = await Booking.findByPk(id);
+    await booking.update({
+        startDate: startDate,
+        endDate: endDate
+    })
+
+    res.json(booking)
+});
+
+
+// Delete an existing booking
+router.delete('/:id', [restoreUser, requireAuthentication, requireAuthorizationUserBooking], async (req, res) => {
+    const user = req.user;
+    const {id} = req.params;
+
+    const booking = await Booking.findByPk(id);
+    await booking.destroy();
+
+    res.json({
+        message: "Successfully deleted",
+        statusCode: 200
+    });
+});
 
 module.exports = router;
